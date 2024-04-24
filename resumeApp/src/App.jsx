2f12db2317bb4b967e0b1f2f65f42779bd550145
Jenkibear <jenkinwork123@gmail.com>
2024-04-24 14:09:08 +1000

    basic framework and routing

--- resumeApp/src/App.jsx
+++ resumeApp/src/App.jsx
@@ -2,32 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/Home'
+import Hobbies from './pages/Hobbies'
+import Education from './pages/Education'
+import SoftSkills from './pages/SoftSkills'
+import TechnicalSkills from './pages/TechnicalSkills'
+import WorkHistory from './pages/WorkHistory'
+import Navbar from './components/Navbar'
+import { Route,Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div >
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/education' element={<Education/>}/>
+        <Route exact path='/hobbies' element={<Hobbies/>}/>
+        <Route exact path='/soft-skills' element={<SoftSkills/>}/>
+        <Route exact path='/technical-skills' element={<TechnicalSkills/>}/>
+        <Route exact path='/work-history' element={<WorkHistory/>}/>
+
+        <Route exact path='*' element={<div>NOT FOUND!!!!</div>}/>
+      </Routes>
+      
+    </div>
+      
     </>
   )
 }
