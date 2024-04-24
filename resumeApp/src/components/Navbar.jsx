bb9391a5a0b92ec91029de513ef7fd1039a5e3dd
Jenkibear <jenkinwork123@gmail.com>
2024-04-24 14:15:48 +1000

    navbar page navigation

--- resumeApp/src/components/Navbar.jsx
+++ resumeApp/src/components/Navbar.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='flex flex-row'>
+      <h3>My Resume React</h3>
+      <ul className='flex flex-row'>
+        <Link to={'/'}>
+          <li>Home</li>
+        </Link>
+        <Link to={'/techinical-skills'}>
+          <li>Technical Skills</li>
+        </Link>
+        <Link to={'/soft-skills'}>
+          <li>Soft Skills</li>
+        </Link>
+        <Link to={'/education'}>
+          <li>Education</li>
+        </Link>
+        <Link to={'/work-history'}>
+          <li>Work History</li>
+        </Link>
+        <Link to={'/hobbies'}>
+          <li>Hobbies</li>
+        </Link>
+      </ul>
+    </div>
   )
 }
