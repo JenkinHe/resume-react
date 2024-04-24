413a15ac1e8c2164bcae3ede44e64ba5fe990f32
Jenkibear <jenkinwork123@gmail.com>
2024-04-24 20:59:04 +1000

    navbar restyling

--- resumeApp/src/components/Navbar.jsx
+++ resumeApp/src/components/Navbar.jsx
@@ -9,22 +9,22 @@ export default function Navbar() {
         </Link>
       <ul className='flex flex-row mx-auto py-3'>
         <Link to={'/'}>
-          <li className='mx-4 inline-block bg-red-600 hover:bg-red-400 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Home</li>
+          <li className='mx-4 inline-block bg-gray-500 hover:bg-red-600 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Home</li>
         </Link>
         <Link to={'/technical-skills'}>
-          <li className='mx-4 inline-block bg-red-600 hover:bg-red-400 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Technical Skills</li>
+          <li className='mx-4 inline-block bg-gray-500 hover:bg-red-600 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Technical Skills</li>
         </Link>
         <Link to={'/soft-skills'}>
-          <li className='mx-4 inline-block bg-red-600 hover:bg-red-400 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Soft Skills</li>
+          <li className='mx-4 inline-block bg-gray-500 hover:bg-red-600 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Soft Skills</li>
         </Link>
         <Link to={'/education'}>
-          <li className='mx-4 inline-block bg-red-600 hover:bg-red-400 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Education</li>
+          <li className='mx-4 inline-block bg-gray-500 hover:bg-red-600 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Education</li>
         </Link>
         <Link to={'/work-history'}>
-          <li className='mx-4 inline-block bg-red-600 hover:bg-red-400 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Work History</li>
+          <li className='mx-4 inline-block bg-gray-500 hover:bg-red-600 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Work History</li>
         </Link>
         <Link to={'/hobbies'}>
-          <li className='mx-4 inline-block bg-red-600 hover:bg-red-400 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Hobbies</li>
+          <li className='mx-4 inline-block bg-gray-500 hover:bg-red-600 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Hobbies</li>
         </Link>
       </ul>
     </div>
