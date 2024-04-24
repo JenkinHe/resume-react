de3c119f3383e15a7e9cf178111fa8bffb1e8d53
Jenkibear <jenkinwork123@gmail.com>
2024-04-24 14:38:32 +1000

    more basic css styling for navbar red and gold with grey background

--- resumeApp/src/components/Navbar.jsx
+++ resumeApp/src/components/Navbar.jsx
@@ -3,28 +3,28 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div className='flex flex-col text-center'>
+    <div className='flex flex-col text-center bg-gray-100'>
       <Link to={'/'}>
-      <h3 className='text-4xl'>My Resume React</h3>
+      <h3 className='text-4xl text-yellow-500 font-bold'>My Resume React</h3>
         </Link>
       <ul className='flex flex-row mx-auto py-3'>
         <Link to={'/'}>
-          <li className='mx-4 inline-block bg-blue-400 hover:bg-blue-300 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Home</li>
+          <li className='mx-4 inline-block bg-red-600 hover:bg-red-400 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Home</li>
         </Link>
         <Link to={'/technical-skills'}>
-          <li className='mx-4 inline-block bg-blue-400 hover:bg-blue-300 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Technical Skills</li>
+          <li className='mx-4 inline-block bg-red-600 hover:bg-red-400 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Technical Skills</li>
         </Link>
         <Link to={'/soft-skills'}>
-          <li className='mx-4 inline-block bg-blue-400 hover:bg-blue-300 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Soft Skills</li>
+          <li className='mx-4 inline-block bg-red-600 hover:bg-red-400 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Soft Skills</li>
         </Link>
         <Link to={'/education'}>
-          <li className='mx-4 inline-block bg-blue-400 hover:bg-blue-300 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Education</li>
+          <li className='mx-4 inline-block bg-red-600 hover:bg-red-400 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Education</li>
         </Link>
         <Link to={'/work-history'}>
-          <li className='mx-4 inline-block bg-blue-400 hover:bg-blue-300 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Work History</li>
+          <li className='mx-4 inline-block bg-red-600 hover:bg-red-400 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Work History</li>
         </Link>
         <Link to={'/hobbies'}>
-          <li className='mx-4 inline-block bg-blue-400 hover:bg-blue-300 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Hobbies</li>
+          <li className='mx-4 inline-block bg-red-600 hover:bg-red-400 text-white rounded-full px-3 py-1 text-2xl font-sans font-open-sans text-center'>Hobbies</li>
         </Link>
       </ul>
     </div>
